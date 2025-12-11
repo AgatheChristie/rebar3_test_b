b204083426f049d3d7e8ce307cb5a5e4d4705da7
yoshioka007 <kuffios@163.com>
2025-12-11 14:15:02 +0800

    awdawd

--- src/cvgame_pre.erl
+++ src/cvgame_pre.erl
@@ -1,12 +1,8 @@
--module(gen_file).
+-module(cvgame_pre).
 
 -export([init/1]).
 
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-    {ok, State1} = gen_file_prv:init(State),
+    {ok, State1} = cvgame_pre_prv:init(State),
     {ok, State1}.
-
--include_lib("kernel/include/file.hrl").
-
-
--- src/cvgame_pre_prv.erl
+++ src/cvgame_pre_prv.erl
@@ -1,9 +1,10 @@
--module(gen_file_prv).
+-module(cvgame_pre_prv).
 
 -export([init/1, do/1, format_error/1]).
 
--define(PROVIDER, gen_file).
+-define(PROVIDER, cvgame_pre).
 -define(DEPS, [app_discovery]).
+
 -include_lib("kernel/include/file.hrl").
 
 %% ===================================================================
@@ -12,16 +13,17 @@
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
     Provider = providers:create([
-        {name, ?PROVIDER},            % The 'user friendly' name of the task
-        {module, ?MODULE},            % The module implementation of the task
-        {bare, true},                 % The task can be run by the user, always true
-        {deps, ?DEPS},                % The list of dependencies
-        {example, "rebar3 game"}, % How to use the plugin
-        {opts, []},                   % list of options understood by the plugin
-        {short_desc, "generate record fields"},
-        {desc, "  生成 "}
+            {name, ?PROVIDER},            % The 'user friendly' name of the task
+            {module, ?MODULE},            % The module implementation of the task
+            {bare, true},                 % The task can be run by the user, always true
+            {deps, ?DEPS},                % The list of dependencies
+            {example, "rebar3 cvgame_pre"}, % How to use the plugin
+            {opts, []},                   % list of options understood by the plugin
+            {short_desc, "A rebar plugin"},
+            {desc, "A rebar plugin"}
     ]),
     {ok, rebar_state:add_provider(State, Provider)}.
+
 
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
@@ -29,6 +31,9 @@ do(State) ->
     io:format("rebar3_test_begin~n"),
     {ok, State}.
 
+
+
 -spec format_error(any()) ->  iolist().
 format_error(Reason) ->
     io_lib:format("~p", [Reason]).
+
